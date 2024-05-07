1cc6a56565a9615e188fb2501a8331086cf583c9
jfrisina <jaki.frisina@gmail.com>
2024-05-07 09:28:55 -0700

    normalize lin endings to LF

---
+++ 308.2/script.js
@@ -0,0 +1,95 @@
+// Exercise 1 ------------------------
+
+// let num = 0;
+
+// if (num > 0) {
+//     console.log(`${num} is greater than 0.`)
+// } else if (num == 0) {
+//     console.log(`${num} is equal to 0.`)
+// } 
+// else {
+//     console.log(`${num} is less than 0.`)
+// }
+
+// Exercise 2 ------------------------
+// let age = 42;
+
+// if (age < 18) {
+//     console.log("Access Denied");
+// } else {
+//     console.log ("Come on in and buy our stuff!");
+//     if (age >= 21) {
+//         console.log('Lucky you, you get to buy our Adult Stuff!')
+//     }
+// }
+
+// Exercise 2 ------------------------
+
+// let num2 = 2;
+
+// if (num2 >= 18 && num2 < 25) {
+//     console.log(`You can rent a car from us!`);
+// } else if (num2 >= 25) {
+//     console.log(`You can rent a car from us and at a discounted price!!`);
+// } else {
+//     console.log(`Lol, go on 'n git outta here`);
+// }
+
+// Exercise 3 -------------------
+//let num3 = 50;
+
+//if (num3 > 0 && num3 < 100)
+
+// Exercise 3 -------------------
+
+// let grade = 24;
+
+// if (grade >= 90) {
+//     console.log("A");
+// } else if (grade >= 80 && grade < 90) {
+//     console.log("B");
+// } else if (grade >= 70 && grade < 80) {
+//     console.log("C");
+// } else if (grade >= 55 && grade < 70) {
+//     console.log("D");
+// } else {
+//     console.log("F");
+// }
+
+// Switch Statement ----------------
+// the break keyword tells JS to get out of the switch statement and not continue down the list. Otherwise, it will keep going
+
+//let pet = 'dog';
+
+// switch(pet) {
+//     case "dog": console.log(`You like doggies`);
+//     break;
+//     case "cat": console.log(`Don't ever invite me to your home because I'm allergic to cats :)`);
+//     break;
+//     case "snake": console.log(`Can I drape him over my shoulders and have my Britney Spears/Megan Thee Stallion moment?`);
+//     break;
+//     case "hamster": console.log(`Omg me too`);
+//     default: (`What's that??`);
+//     break;
+// }
+
+// Ternary ----------------------
+
+// let x = 2;
+// let isTen;
+
+// if (x == 10) {
+//     isTen = true;
+// } else {
+//     isTen = false;
+// }
+// console.log(isTen);
+
+
+
+// let y = 8;
+
+// let isEight = y == 8 ? true : false;
+// console.log(isEight);
+
+// Errors ----------------------
